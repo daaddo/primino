7cc1a263d59e261dca1e06a41430ed726d97798c
Luca <luca.coraci@gmail.com>
2022-04-04 19:17:01 +0200

    version

---
+++ src/main/java/it/daddo/primino/Settings.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.daddo.primino;
+
+/**
+ *
+ * @author Luca
+ */
+public class Settings {
+    
+    public static final String version = "1.0";
+    
+}
